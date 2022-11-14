92ad7b4c03e730bdca768d2eb5ec792f85444b59
eminkabacaoglu <eminkabacaoglu@gmail.com>
2022-11-14 17:29:55 +0300

    first commit

---
+++ src/main/java/com/atk/tennisAcademy/TennisAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.atk.tennisAcademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisAcademyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atk/tennisAcademy/TennisAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atk.tennisAcademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
