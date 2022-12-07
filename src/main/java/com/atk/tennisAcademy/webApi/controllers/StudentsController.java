88e5c918c92dc6f827dd0fd76c3274c5ee34382b
eminkabacaoglu <eminkabacaoglu@gmail.com>
2022-12-08 00:31:27 +0300

    update methods are added

---
+++ src/main/java/com/atk/tennisAcademy/webApi/controllers/StudentsController.java
@@ -0,0 +1,44 @@
+package com.atk.tennisAcademy.webApi.controllers;
+
+import com.atk.tennisAcademy.business.abstracts.StudentService;
+import com.atk.tennisAcademy.entities.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/students")
+public class StudentsController {
+
+    @Autowired
+    StudentService studentService;
+
+    @GetMapping
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudents();
+    }
+
+    @GetMapping("/{id}")
+    public Student getStudent(@PathVariable Long id){
+        return studentService.getStudentById(id);
+    }
+
+    @PostMapping
+    public Student saveStudent(@RequestBody Student student){
+        return studentService.saveStudent(student);
+    }
+
+    @PutMapping("/{id}")
+    public Student updateStudent(@PathVariable Long id, @RequestBody Student student){
+        return studentService.updateStudent(id ,student);
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean deleteStudent(@PathVariable Long id){
+        return studentService.deleteStudent(id);
+    }
+
+
+}
