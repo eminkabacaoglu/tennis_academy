098fd4bf3b19eca82cebd21029938b3a49d0d144
eminkabacaoglu <eminkabacaoglu@gmail.com>
2023-04-04 02:17:21 +0300

    student entity updated

--- src/main/java/com/atk/tennisAcademy/entities/Student.java
+++ src/main/java/com/atk/tennisAcademy/entities/Student.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @Data
@@ -16,15 +15,25 @@ import java.util.List;
 @Table(name = "students")
 public class Student extends Person{
     private String school;
+    private String parentNameSurname;
+    private String parentJob;
+    private String parentWorkPlace;
+    private String alternativeParent;
     private boolean isInBlacklist=false;
+
+    private LocalDate dateOfRegisteration;
 
     public String blackListDescription="";
     @Enumerated(EnumType.STRING)
     private AttendanceType attendanceType;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "student_type_id")
     private StudentType studentType;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "level_id")
+    private Level level;
 
     @ManyToMany(cascade =CascadeType.ALL,fetch=FetchType.EAGER)
     @JoinTable(
@@ -35,11 +44,17 @@ public class Student extends Person{
             referencedColumnName = "id"))
     private List<Member> members;
     @Builder
-    public Student(String firstName, String lastName, String username, String password, String nationalId, LocalDate dateOfBirth, String placeOfBirth, Gender gender, String mobilePhone, String homePhone, String email, String photoUrl,City city,County county, String createdBy, String school, AttendanceType attendanceType, StudentType studentType) {
+    public Student(String firstName, String lastName, String username, String password, String nationalId, LocalDate dateOfBirth, String placeOfBirth, Gender gender, String mobilePhone, String homePhone, String email, String photoUrl,City city,County county, String createdBy, String school, AttendanceType attendanceType, StudentType studentType,Level level,LocalDate dateOfRegisteration,String parentNameSurname,String parentJob,String parentWorkPlace,String alternativeParent) {
         super(firstName, lastName, username, password, nationalId, dateOfBirth, placeOfBirth, gender,mobilePhone,homePhone,email,photoUrl,city,county,createdBy);
         this.school = school;
         this.attendanceType = attendanceType;
         this.studentType=studentType;
+        this.level = level;
+        this.dateOfRegisteration=dateOfRegisteration;
+        this.parentNameSurname=parentNameSurname;
+        this.parentJob = parentJob;
+        this.parentWorkPlace = parentWorkPlace;
+        this.alternativeParent = alternativeParent;
     }
     public enum AttendanceType{
         General{
@@ -52,7 +67,7 @@ public class Student extends Person{
                 return "Tam Gün";
             }
         },
-        Half_time{
+        Half_Time {
             public String toString() {
                 return "Yarım Gün";
             }
